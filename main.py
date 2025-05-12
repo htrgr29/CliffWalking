49e2eaa34e05c7cafb0550df5c5a65e9f9331f6b
jacekrudek <115008121+jacekrudek@users.noreply.github.com>
2025-05-13 00:54:17 +0200

    basic fitness function & RNG seed

--- main.py
+++ main.py
@@ -4,33 +4,55 @@ import gymnasium as gym
 from enum import IntEnum
 from inidvidual import Individual
 
+#enum for directions
 class Dir(IntEnum):
     UP = 0
     DOWN = 2
     LEFT = 3
     RIGHT = 1
 
+#init gymnasium CliffWalking
 env = gym.make("CliffWalking-v0", render_mode="human")
-observation, info = env.reset()
+
+#variable declaration
+POPULATION_SIZE = 150
+MOVES_QUANTITY = 18
+WIDTH = 12
+
+random.seed(0)
+state, info = env.reset()
 env.render()
 
 #function for getting random dir
 def getRandomDir():
     random.choice(list(Dir))
 
+#function for calculating fitness
+def calculateFitness(row, col, movesCount):
+    rowDifference = 3 - row
+    colDifference = 12 - col
+    return 50 - rowDifference - colDifference - movesCount
+
 #create initial population
-population = [Individual([random.randint(0, 3) for _ in range(18)], 0, False) for _ in range(150)]
+population = [Individual([random.randint(0, 3) for _ in range(MOVES_QUANTITY)], -1, False) for _ in range(POPULATION_SIZE)]
 
 iterator = -1
+movesCount = 0
 
-for i in population:
+row = 3
+col = 0
+
+for individual in population:
     iterator += 1
-    print()
     print(str(iterator) + ": ", end="")
-    for j in i.moves:
-        print(str(j) + ", ", end="")
-
-env.step(Dir.UP)
-env.step(Dir.RIGHT)
-
-env.action_space.seed(0)
+    for move in individual.moves:
+        movesCount += 1
+        state, *_ = env.step(move)
+        row = state // WIDTH
+        col = state % WIDTH
+        print(str(move) + ", ", end="")
+    print()
+    individual.fitness = calculateFitness(row, col, movesCount)
+    print("fitness:" + str(individual.fitness))
+    env.reset()
+    movesCount = 0
